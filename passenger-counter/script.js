44c71b0b134d4a569761547a33bbbb0969455dbb
GianniKilkenny <giannikilkenny22404@gmail.com>
2025-08-11 15:40:53 -0700

    counter and save funcitons added

--- passenger-counter/script.js
+++ passenger-counter/script.js
@@ -1,6 +1,28 @@
 // document.getElementById("count").innerText = 5;
 
+// let firstBatch = 5;
+// let secondBatch = 7;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
+
+// let myAge = 21;
+// let humanDogRatio = 7 / 1;
+
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge);
+
 let count = 0;
 
-let myAge = 21;
-console.log(myAge);
+function increment() {
+  count++;
+  let c = document.getElementById("count");
+  c.textContent = count;
+}
+
+function save() {
+  console.log(`Your count is saved at ${count}`);
+}
+save;
