facc57c196444cd09d16819787b7840377d327d9
GianniKilkenny <giannikilkenny22404@gmail.com>
2025-08-12 13:03:29 -0700

    Blackjack basics, also added some css

---
+++ black-jack/script.js
@@ -0,0 +1,29 @@
+let firstCard = 11;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+
+if (sum <= 20) {
+  message = "do you want to draw a new card lil bro?";
+} else if (sum === 21) {
+  message = "just take the bread lil bro";
+  let hasBlackJack = true;
+} else {
+  message = "damn i know yo pockets finna be hurting lil bro";
+  let isAlive = false;
+}
+
+console.log(message);
+
+// let age = 100;
+
+// if (age < 100) {
+//   console.log("You still a youngin");
+// } else if (age === 100) {
+//   console.log("Here is your bitrthday card from the King!");
+// } else {
+//   console.log("Man stop cappin you already got the shit you old ass mf");
+// }
